0df3d1696e361c831d7f5db8b922ab1c05bafc50
Sigbjorn Lie <45419145+sigbjornaib@users.noreply.github.com>
2020-03-09 15:22:31 +0100

    134 Add enhancement for purging unmanaged firewalld zones

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,7 +50,7 @@ class firewalld (
   Boolean                                                       $purge_direct_chains       = false,
   Boolean                                                       $purge_direct_passthroughs = false,
   Boolean                                                       $purge_unknown_ipsets      = false,
-  Boolean $purge_zones               = false,
+  Boolean                                                       $purge_zones               = false,
   Optional[String]                                              $default_zone              = undef,
   Optional[Enum['off','all','unicast','broadcast','multicast']] $log_denied                = undef,
   Optional[Enum['yes', 'no']]                                   $cleanup_on_exit           = undef,
